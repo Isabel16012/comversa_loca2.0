c6ae8b2abac904ade6b636eb58c6a45d894ea271
Isabel16012 <119525584+Isabel16012@users.noreply.github.com>
2023-07-12 15:58:46 -0300

    Add files via upload

---
+++ conversa_loca/main.js
@@ -0,0 +1,14 @@
+function chamar_pagina()
+{
+    window.open("curiosidades.html");
+}
+
+function chamar_pagina2()
+{
+    window.open("curi2.html");
+}
+
+function chamar_pagina3()
+{
+    window.open("curi3.html");        
+}
